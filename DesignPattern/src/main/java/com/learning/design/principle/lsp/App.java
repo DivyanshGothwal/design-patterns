dc174e10a20bd8af800d00ffd1f2abe0cc49ce3d
Divyansh Gothwal <shotsdivyansh@gmail.com>
2021-02-20 19:32:29 +0530

    updated comment

--- DesignPattern/src/main/java/com/learning/design/principle/lsp/App.java
+++ DesignPattern/src/main/java/com/learning/design/principle/lsp/App.java
@@ -3,8 +3,15 @@ package com.learning.design.principle.lsp;
 public class App {
 
 	public static void main(String ar[]) {
-		// here Duck and ostrish can be replaced without any issue with their respective
-		// super/parent type
+		/*
+		 * here Duck and ostrish can be replaced without any issue with their respective
+		 * super/parent type
+		 * 
+		 * Where as if there has been only bird type having fly method then then
+		 * ostrtish fly method would have thrown error which violates liskovs
+		 * substitution principle
+		 */
+
 		Bird d = new Duck();
 		d.walk();
 		Bird o = new Ostrish();
